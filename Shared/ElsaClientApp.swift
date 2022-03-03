b8d27e441fd8c419be2fe045e27bfa99dc24a431
Daniel Caris <dcarisz@ripley.com>
2022-03-03 10:35:26 -0300

    Initial Commit

---
+++ Shared/ElsaClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElsaClientApp.swift
+//  Shared
+//
+//  Created by Daniel Caris on 03-03-22.
+//
+
+import SwiftUI
+
+@main
+struct ElsaClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
